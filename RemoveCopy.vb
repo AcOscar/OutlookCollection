a5fc430ceea5618b5e9f10e09ad28224c058b630
BslOscar <holger.rasch@gmail.com>
2023-01-03 10:50:20 +0100

    Update RemoveCopy.vb

--- RemoveCopy.vb
+++ RemoveCopy.vb
@@ -18,52 +18,40 @@ iItemsUpdated = 0
 Itemscount = 0
 ProcessFolder calendar
 
-
-Rem For Each Item In calendar.Items
-Rem    If Mid(Item.Subject, 1, 6) = "Copy: " Then
-    Rem If Mid(Item.Subject, 1, 6) = "" Then
-Rem    If Item.Subject = "" Then
-      Rem strTemp = Replace(Item.Subject, "Copy: ", "")
-      Rem Item.Subject = strTemp
-Rem      Item.Subject = " "
-Rem      iItemsUpdated = iItemsUpdated + 1
-Rem    End If
-Rem    Item.Save
-Rem Next Item
-
 MsgBox iItemsUpdated & " of " & Itemscount & " Meetings Updated"
 
 End Sub
 
 
 Private Sub ProcessFolder(ByVal oParent As MAPIFolder)
-Rem For Each subf In folder.Folders
-Rem    removeinfolder (subf)
-Rem Next subf
+
 Dim olFolder As Outlook.Folder
 
- Dim strTemp As String
- Dim Item As Object
- If (oParent.Folders.Count > 0) Then
+Dim strTemp As String
+Dim Item As Object
+If (oParent.Folders.Count > 0) Then
      For Each olFolder In oParent.Folders
          ProcessFolder olFolder
      Next
- End If
-
+End If
 
 Itemscount = oParent.items.Count + Itemscount
 
- For Each Item In oParent.items
-     Rem If Mid(Item.Subject, 1, 6) = "Copy: " Then
+For Each Item In oParent.items
+    
+     If Mid(Item.Subject, 1, 6) = "Copy: " Then
+       strTemp = Replace(Item.Subject, "Copy: ", "")
+       Item.Subject = strTemp  
+       iItemsUpdated = iItemsUpdated + 1
+       Item.Save
+     End If
+    
      If Item.Subject = "" Then
-       Rem strTemp = Replace(Item.Subject, "Copy: ", "")
-       Rem Item.Subject = strTemp
        Item.Subject = " "
        iItemsUpdated = iItemsUpdated + 1
        Item.Save
-        
      End If
-     
- Next Item
+ 
+Next Item
 
 End Sub
